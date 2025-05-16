b8817f236b452954d6c3bcb174993aae9286f705
William Matkaselkä <william.matkaselka@hotmail.com>
2025-05-16 22:37:03 +0200

    fix get_user_by:email method

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -30,6 +30,8 @@ class UserController(Controller):
 
         try:
             users = self.dao.find({'email': email})
+            if len(users) == 0:
+                return None
             if len(users) == 1:
                 return users[0]
             else:
